55f6a2526024ff902c9da33bee3f137f34e69dc0
Eudis Alves <deleudis@gmail.com>
2023-02-28 21:53:43 -0300

    iniciando o js

--- css/js/js/main.js
+++ css/js/js/main.js
@@ -1,1 +1,21 @@
-// 18bd9447eefe8ff200cce9e260f5c339
+//Variaveis
+
+const apikey = "18bd9447eefe8ff200cce9e260f5c339";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+// Funções
+
+
+
+// Eventos
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    console.log("city");
+});
